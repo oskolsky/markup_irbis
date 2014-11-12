c86648ac3d6d6af8c0d5acd7bdb355cb36248071
oskolsky <mo@coalla.ru>
2014-11-12 09:58:19 +0300

    Load materials & off

--- build/assets/javascripts/feed.js
+++ build/assets/javascripts/feed.js
@@ -1,13 +1,22 @@
 $(function() {
 
+  var i = 0;
+
   $('.js-feed-load').on('click', function() {
-    
+
+    i++;
+
     var
-      destination = $('.works > .works_i:last-child').offset().top - $('#header').outerHeight();
+      destination = $('.works > .works_i:last-child').offset().top - $('.header_inner').outerHeight();
 
     $('html, body').animate({scrollTop: destination}, 500);
 
     setTimeout('loadFeed()', 400)
+
+    if (i == 2) {
+      $('.feed-load').remove();
+      $('.works').addClass('works__off');
+    }
 
     return false;
   });
